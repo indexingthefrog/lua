cc88ead786ab62f1f3061c0292b2d677d598f464
brian <116122906+indexingthefrog@users.noreply.github.com>
2022-10-19 15:23:25 -0400

    Update sha256 + salt.lua

--- sha256 + salt.lua
+++ sha256 + salt.lua
@@ -1,42 +1,31 @@
--- creds to dontbanimtoocool for the api link <3
 local msg = 'hello'
 local numbs = '12345'
-local url = "api.hashify.net/hash/sha256/hex?value=" .. msg
-local request = request or http.request or syn.request
+local hash = crypt.hash or syn.crypt.hash
+local type = type or typeof
+local char = string.char 
+local sub = string.sub
+local lower = string.lower
 
-function sha256(message)
-    if typeof(message) == 'string' then
-        local r = request(
-            {
-                Url = url,
-                Method = "GET",
-                Headers = {
-                    ["Content-Type"] = "application/json",
-                    ["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-                },
-            Body = game:GetService("HttpService"):JSONEncode({text = message})
-            }
-        )
-    return r.Body
+function sha256(k) 
+    if type(k) == 'string' and k~=nil and k~='' then 
+      return hash(k,'sha256')  
     end
 end
-
 
 function salt(len) 
     local res = ''
         if typeof(length) ~= 'string' and len > 0 then 
             for i = 1,len do 
-                res = res .. string.char(math.random(97, 122)) .. string.sub(numbs,math.random(1,#numbs))
+                res = res .. char(math.random(97, 122)) .. sub(numbs,math.random(1,#numbs))
             end
         return res
     end
 end
-
-
-local slt = salt(25)
-local mg = msg..slt
+local slt = salt(64)
+local mg = msg .. slt
 local sha = sha256(mg)
 
+
 print('salt id output : ' .. mg)
-print('sha256 output (copied to clipboard!): ' .. sha) -- example: hello + salt = 412a108e202c0956bb7f3c5e5708ff2ecd38052feb87062a26ec0ca121a71338
-setclipboard(sha)
+print('sha256 output (copied to clipboard!): ' .. lower(sha)) -- example: hello + salt = 412a108e202c0956bb7f3c5e5708ff2ecd38052feb87062a26ec0ca121a71338
+setclipboard(lower(sha))
