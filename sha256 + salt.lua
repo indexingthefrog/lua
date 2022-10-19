06c9d0c2b615084abe223c4767e0a57f488863e4
brian <116122906+indexingthefrog@users.noreply.github.com>
2022-10-19 12:15:15 -0400

    Update sha256 + salt.lua

--- sha256 + salt.lua
+++ sha256 + salt.lua
@@ -38,5 +38,5 @@ local mg = msg..slt
 local sha = sha256(mg)
 
 print('salt id output : ' .. mg)
-print('sha256 output : ' .. sha) -- example: hello + salt = 412a108e202c0956bb7f3c5e5708ff2ecd38052feb87062a26ec0ca121a71338
-setclipboard('copied to clipboard! : ' .. sha)
+print('sha256 output (copied to clipboard!): ' .. sha) -- example: hello + salt = 412a108e202c0956bb7f3c5e5708ff2ecd38052feb87062a26ec0ca121a71338
+setclipboard(sha)
