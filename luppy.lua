9de19d85a1b465a597953ec9df221f9608c6d882
brian <116122906+indexingthefrog@users.noreply.github.com>
2022-10-20 09:43:39 -0400

    Create luppy.lua

---
+++ luppy.lua
@@ -0,0 +1,17 @@
+local py = {}
+local mod = math.fmod
+-- https://cboard.cprogramming.com/c-programming/150505-rounding-up.html
+function py.round(v,nearest) 
+    local m,r=1
+    if v and type(v) == 'number' and v>0 then 
+        for i = 0,nearest do 
+            m=m*i    
+        end
+        v=v*m;v=v+0.5;v=v/m
+    end
+    return math.ceil(v)
+end
+
+print(py.round(100,10))
+
+return py
